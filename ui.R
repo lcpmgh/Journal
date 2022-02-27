e9d80829c629de3c9f9ccc5bb94000170c4f95ae
lcpmgh <1030606901@qq.com>
2022-02-27 16:11:19 +0800

    更新了shiny

--- ui.R
+++ ui.R
@@ -16,5 +16,18 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(width=3, uiOutput("ui_sidebar")),
     mainPanel(uiOutput("ui_main"))
+  ),
+  tags$hr(),
+  tags$div(align = "center", 
+           tags$p("--- Designed by PMGH ---"),
+           actionLink(inputId = "", label = "Github", icon = icon("github"), onclick ="window.open('https://github.com/lcpmgh/Journal')"),
+           tags$p("  ", style = "display:inline;white-space:pre"),
+           tags$p("Email: lcpmgh@gmail.com", style="display:inline;white-space:pre"),  
+           tags$div(align = "center",
+                    # tags$img(src="http://www.beian.gov.cn/portal/download"),
+                    # tags$a("冀ICP备2022003075号", target="_blank", href="https://beian.miit.gov.cn", style="color:#06c; display:inline;"),
+                    # tags$p("  ", style = "display:inline;white-space:pre"),
+                    tags$a("冀ICP备2022003075号", target="_blank", href="http://beian.miit.gov.cn", style="color:#06c; display:inline;")
+           )
   )
 )
